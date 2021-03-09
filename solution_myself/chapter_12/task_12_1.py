7f38abbd553899e4d190a9074847506b80908a68
apterek <danialsud@gmail.com>
2021-03-09 19:14:37 +0300

    update

--- solution_myself/chapter_12/task_12_1.py
+++ solution_myself/chapter_12/task_12_1.py
@@ -1,15 +1,21 @@
-from os import subprocess
+import subprocess
 
 
-#def ping_ip_addresses(spisok_ip):
-#    result = []
-#    for ip in spisok_ip:
-#        result = 
+def ping_ip_addresses(spisok_ip):
+    available = []
+    not_available = []
+    for ip in spisok_ip:
+        reply = subprocess.run(['ping', '-n', '1', ip ], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        print(reply.returncode)
+        if reply.returncode == 0:
+            available.append(ip)
+            
+        else:
+            not_available.append(ip)            
+    return available, not_available
 
 
+ip_for_check = ['8.8.8.8', '1.1.1.1', 'ya.ru', '192.168.101.1']
 
-#    return result
-
-result = subprocess.run(['ping','8.8.8.8'])
-
-print(result)
+if __name__ == "__main__":  
+    print(ping_ip_addresses(ip_for_check))
